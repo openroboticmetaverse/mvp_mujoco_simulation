ad9723eda5b0f4b773d1fb29a414a4a36eb86c59
Yoseob Shim <yoseobshim9695@gmail.com>
2024-07-05 10:54:00 +0200

    Revert "feat: add entrypoint feature"
    This reverts commit 4ba9b01e651c6810344e43f8b15391c158a481ca.

--- docker/env.sh
+++ docker/env.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-set -e
-
-echo "Creating Python virtual environment"
-python3 -m venv myEnv
-
-echo "Installing python packages"
-./myEnv/bin/pip install -r /sim_ws/requirements.txt
-
-echo "Start simulation"
-./myEnv/bin/python3 /sim_ws/src/mujoco_simulation.py
+#!/usr/bin/bash
+echo "Activating Local Env"
+# Activate the virtual environment
+source myEnv/bin/activate
